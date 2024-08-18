835ccd52a0b33b9a0e37366a5e10082fcc326894
Nataliia Hordiienko <Natalihordienko@gmail.com>
2024-08-18 23:48:58 +0200

    added markup and counter logic

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const [showFeedback, setShowFeedback] = useState(false);
+  const [showReset, setShowReset] = useState(false);
+
+  const updateFeedback = (feedbackType) => {
+    console.log("click", feedbackType);
+    setFeedback({ ...feedback, [feedbackType]: feedback[feedbackType] + 1 });
+    setShowFeedback(true);
+    setShowReset(true);
+  };
+
+  const total = feedback.good + feedback.neutral + feedback.bad;
+  const positive = Math.round((feedback.good / total) * 100);
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+
+    setShowFeedback(false);
+    setShowReset(false);
+  };
+
+  return (
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        showReset={showReset}
+      />
+
+      {!showFeedback ? (
+        <Notification />
+      ) : (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={total}
+          positive={positive}
+        />
+      )}
+    </>
+  );
+}
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,38 @@
+import css from "./Options.module.css";
+
+export default function Options({ updateFeedback, resetFeedback, showReset }) {
+  return (
+    <div className={css.options}>
+      <button
+        className={css.button}
+        type="button"
+        onClick={() => updateFeedback("good")}
+      >
+        Good
+      </button>
+      <button
+        className={css.button}
+        type="button"
+        onClick={() => updateFeedback("neutral")}
+      >
+        Neutral
+      </button>
+      <button
+        className={css.button}
+        type="button"
+        onClick={() => updateFeedback("bad")}
+      >
+        Bad
+      </button>
+      {showReset && (
+        <button
+          className={css.button}
+          type="button"
+          onClick={() => resetFeedback("reset")}
+        >
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
